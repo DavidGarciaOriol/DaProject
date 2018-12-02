2172e932f294809453026da4f7202b49151adadc
David García Oriol <davidgarciaoriol@gmail.com>
2018-12-02 02:16:40 +0100

    Some Things Modified

--- html/includes/mainpage.php
+++ html/includes/mainpage.php
@@ -1,20 +1,20 @@
 <div class="maingrid">
 
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
-    <div class="maingriditem"></div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
+    <div class="maingriditem">TEST TEST TEST TEST TEST</div>
 
 </div>
