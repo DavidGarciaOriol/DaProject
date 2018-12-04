b8c589c99d0dc678e11d9f60fc6b4c37e21ec853
David García Oriol <davidgarciaoriol@gmail.com>
2018-12-04 05:18:57 +0100

    Main Page Workin'... Almost

--- html/includes/maingridpage.php
+++ html/includes/maingridpage.php
@@ -1,23 +1,27 @@
 <div class="indexgrid">
 
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
+    <div class="indexgriditem">
+        <div class="itemcard">
+            <h4> TEST TEST TEST </h4>
+        </div>
+    </div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
 
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
 
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
 
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
-    <div class="indexgriditem"><h2>TEST TEST TEST TEST TEST</h2></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
+    <div class="indexgriditem"></div>
 
 </div>
