a859355857d790d6bfd84e304f6429c2976a9a9f
David García Oriol <davidgarciaoriol@gmail.com>
2018-12-15 04:27:09 +0100

    info, discord, rooms, daButoni...

--- html/includes/footerPages/infoBody.php
+++ html/includes/footerPages/infoBody.php
@@ -1,10 +1,10 @@
+<div id="infomsg"></div>
 
 <div class="infoindex">
     <ul>
         <li> <a href="#funcionamiento"> Funcionamiento </a> </li>
-        <li> <a href="#funcionamiento2"> Info </a> </li>
-        <li> <a href="other"> Lorem </a> </li>
-        <li> <a href="other2"> Ipsum </a> </li>
+        <li> <a href="#comunidades"> Comunidades </a> </li>
+        <li> <a href="#informacion"> Información </a> </li>
         
     </ul>
 </div>  
@@ -12,27 +12,114 @@
 
 <div class="infomsg">
     
-<h2>
-    <strong> INFORMACIÓN IMPORTANTE </strong>
-</h2>
-
 <p>
+<i>
     Esta aplicación pretende crear un entorno en el cual puedas escuchar la música que te apetezca con quien te apetezca. <br>
     Sin líos ni movidas de por medio. Un click es lo máximo que necesitas. <br> <br>
+</i>
+</p>
+    
+
+
+<h2 id="funcionamiento">
+    <strong> FUNCIONAMIENTO </strong>
+</h2>
+
+<h3> <strong> Crear Sala </strong> </h3>
+
+<p>
+    En la página principal, solo tienes que seleccionar un género, y se creará una sala automáticamente. <br>
+    Tras ello, con el enlace de la sala podrás invitar a quien quieras a la misma. <br>
     
 </p>
 
-<h2 id="funcionamiento">
-    FUNCIONAMIENTO
-</h2>
-
-<h3> General </h3>
+<h3> <strong> Control de la página </strong> </h3>
 
 <p>
-    Su empleo es sencillo. Nada mas entrar, solo tienes que seleccionar un género, y se creará una sala automáticamente. <br>
-    Tras ello, con el enlace de la sala podrá invitar a quien quiera a la misma. <br>
-    También se puede acceder a los distintos apartados, como tu perfil, o crear/buscar/entrar en una comunidad desde la página <br>
-    principal.
+    Desde la página principal se puede acceder a las páginas para identificarse o registrarse, así como <br>
+    a la página de tu cuenta en caso de estar identificado. Todo esto se encuentra en el encabezado de la web. <br> <br>
+
+    En el pie de la página, se encuentra todo lo relacionado a la información de la misma, <br>
+    del creador, o contacto en las redes sociales de éste (o sea yo... Hola mamá). <br>
+
+</p>
+
+<h3> <strong> Control de Sala </strong> </h3>
+
+<p>
+    En una sala, introduciendo un enlace en la barra de búsqueda situada junto al reproductor <br>
+    podrás ser capaz de reproducir el vídeo musical que plazcas. También cuenta con un chat en el <br>
+    margen derecho de la página, y un sencillo control sobre los otros usuarios en <br>
+    la sala, así como información sobre ésta, como el género, o lo que está sonando actualmente. <br> <br>
+
+    Una vez la sala se quede sin usuarios, ésta será borrada instantáneamente. <br>
+    Siempre se puede crear una nueva desde la página principal siguiendo el mismo método que con la anterior. <br>
+
+</p>
+
+<h3> <strong> Control del Chat </strong> </h3>
+
+<p>
+    El chat es una herramienta sencilla para comunicarte con los usuarios presentes en la sala actual. <br>
+    Dispone de una pestaña principal donde poder enviar y ver mensajes de otros usuarios y una pestaña <br>
+    para ver los usuarios actualmente en línea. <br> <br>
+
+    Tan sencillo como escribir en la caja inferior, pulsar intro o darle click a "Enviar" y tu mensaje será enviado. <br><br>
+
+</p>
+
+<h2 id="comunidades"> <strong> COMUNIDADES </strong> </h2>
+
+<h3> <strong> ¿Qué son? </strong> </h3>
+
+<p>
+    Se trata de una manera de unificar varios usuarios con gustos e intereses comunes, en un mismo espacio. <br>
+    
+</p>
+
+<h3> <strong> ¿Cómo creo una? </strong> </h3>
+
+<p>
+    Si eres un usuario registrado e identificado en la web, tendrás acceso al botón "Comunidades" situado en la <br>
+    parte superior de ésta. <br> <br>
+
+    Con esta opción, se abrirá un menú para crear una comunidad, o gestionar una existente. Basta con introducir el nombre y <br>
+    una descripción, y la comunidad ya estaría en marcha, siendo tú el líder de ésta. <br> <br>
+
+    Una vez creada, en el mismo apartado ya mencionado, podrás gestionar algunos detalles de la misma, como su <br>
+    nombre, su descripción o sus miembros. <br>
+</p>
+
+<h3> <strong> ¿Cómo invito a alguien a mi comunidad? </strong> </h3>
+
+<p>
+    Dentro del gestor de la comunidad, podrás generar un enlace de invitación a la misma, para dárselo a <br>
+    quien quieras que desee entrar a ella. Tan sencillo como eso. <br> <br>
+
+    En dicho gestor, puedes gestionar a los miembros de tu comunidad, así como expulsarlos o pasarles el liderazgo<br>
+    de ésta. <br> <br>
+
+</p>
+
+
+<h2 id="informacion"> <strong> INFORMACIÓN Y ACTUALIZACIONES </strong> </h2>
+
+<h3> <strong> Desarrollo </strong> </h3>
+
+<p>
+    La página se encuentra en una fase temprana de su estado de desarrollo, y será actualizada hasta el <br>
+    punto de poder ser lanzada al público lo antes posible.<br> <br>
+
+    Se trata de un proyecto que llevo mucho tiempo queriendo desarrollar y probar, y sé que poco a poco, <br>
+    podré conseguir algo cercano a mi meta, y con el tiempo superarme. <br>
+
+<p>
+
+<h3> <strong> Actualizaciones </strong> </h3>
+
+<p>
+    La página será actualizada regularmente, añadiendo nuevas funcioanlidades y mejoras <br>
+    respaldadas por las peticiones de la comunidad y las necesidades de comodidad de los usuarios. <br>
 </p>
 
 </div>
---
+++ html/includes/upToButtom.php
@@ -0,0 +1,3 @@
+<div class="upButtom">
+    <a class="upper" href="#infomsg"> UP </a>
+</div>
